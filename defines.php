0b619b58a55ebb607d3630a8d7372e4220bb39c8
Vinay Kumar Srivastava <vinaysrivastava273@gmail.com>
2021-05-22 16:25:25 +0530

    For IG Basic Display API

---
+++ defines.php
@@ -0,0 +1,4 @@
+<?php
+	define( 'INSTAGRAM_APP_ID', '153190273446618' );
+	define( 'INSTAGRAM_APP_SECRET', '86d11c7be7979869c9300cd3c4066' );
+	define( 'INSTAGRAM_APP_REDIRECT_URI', 'https://vinaysrivastava273.github.io/vinay.github.io/contact.html' );
